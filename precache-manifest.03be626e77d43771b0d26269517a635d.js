0f762ffa52b21cba5c8b5b5ffffcafbda46ef5e4
dark <tuan150419999@gmail.com>
2020-04-23 11:59:27 +0700

    Updates

---
+++ precache-manifest.03be626e77d43771b0d26269517a635d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b3537c3ea6e6a04ee25448629f9655",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "fde135aa421d75b13e68",
+    "url": "/todo-list/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a29f0c7f5b1184821fba",
+    "url": "/todo-list/static/js/2.6037fe85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.6037fe85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fde135aa421d75b13e68",
+    "url": "/todo-list/static/js/main.87c24e27.chunk.js"
+  },
+  {
+    "revision": "9c00833b61171e0ba36d",
+    "url": "/todo-list/static/js/runtime-main.c17e1442.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
